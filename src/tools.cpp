578e2a0e21c07b896466bef4f94924568846f5bb
john-reilly <johnreillymct@outlook.com>
2018-12-01 17:55:25 +0000

    feat add code to CalculateRMSE from Term 2 Project 1 EKF.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,4 +15,39 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+    // this is a straight copy from my EKF.cpp project file see link
+  // https://github.com/john-reilly/Self_Driving_Car_Nano_Degree_Term_2_Project_1_Extended_Kalman_Filter/blob/master/src/tools.cpp
+  
+  
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+  // check the validity of the following inputs:
+  //  * the estimation vector size should not be zero
+  //  * the estimation vector size should equal ground truth vector size
+  if(estimations.size() != ground_truth.size()
+      || estimations.size() == 0){
+      cout << "Invalid estimation or ground_truth data" << endl;
+      return rmse;
+  }
+
+  //accumulate squared residuals
+  for(unsigned int i=0; i < estimations.size(); ++i){
+
+      VectorXd residual = estimations[i] - ground_truth[i];
+
+      //coefficient-wise multiplication
+      residual = residual.array()*residual.array();
+      rmse += residual;
+   }
+
+  //calculate the mean
+  rmse = rmse/estimations.size();
+
+  //calculate the squared root
+  rmse = rmse.array().sqrt();
+
+  //return the result
+return rmse;
+  
 }
